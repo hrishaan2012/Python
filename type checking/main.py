842f96f38797835ad3f6a1dda125f1c729570426
hrishaan2012 <hrishaan2012@gmail.com>
2025-06-18 20:38:02 +0530

    type conversion

--- type checking/main.py
+++ type checking/main.py
@@ -6,3 +6,13 @@ c=False
 print("Type of c:", type(c))
 d="Hello, World!"
 print("Type of d:", type(d))
+
+print("\n After type conversion")
+a=float(a)
+print( "type of a is",type(a), "value of a is", a)
+b=int(b)
+print( "type of b is",type(b), "value of b is", b)
+c=str(c)
+print( "type of c is",type(c), "value of c is", c)
+d=bool(d)
+print( "type of d is",type(d), "value of d is", d)
