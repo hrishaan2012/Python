fc1c4177cf4cc093342b808db0f583eafcb1f19d
hrishaan2012 <hrishaan2012@gmail.com>
2025-06-25 20:40:57 +0530

    greater smaller

---
+++ greater smaller/main.py
@@ -0,0 +1,7 @@
+no1=int(input("Enter the first number:"))
+no2=int(input("Enter the second number:"))
+if no1<no2:
+    print ("The first number is smaller than the second number ")
+else:
+    print("The first number is greater than the second number")
+    
