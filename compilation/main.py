f4778880b1f0cea21f09a247a309884bf3075115
hrishaan2012 <hrishaan2012@gmail.com>
2025-06-26 20:37:08 +0530

    compilation

---
+++ compilation/main.py
@@ -0,0 +1,19 @@
+a=20
+b=30
+c=30
+
+print(a!=b)
+print(b!=c)
+
+print("a and b are different")
+
+a=4
+b=6
+if (a==3)!= (b==6):
+    print("hello")
+
+    a=int(input("Enter a Number to check odd or even: "))
+    if a%2==0:
+        print("Even")
+    else:
+        print("Odd")
