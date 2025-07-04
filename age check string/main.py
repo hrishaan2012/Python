d86a26bd8538c499dcf427a40d9d25382f044b59
hrishaan2012 <hrishaan2012@gmail.com>
2025-07-04 21:57:31 +0530

    age check string

---
+++ age check string/main.py
@@ -0,0 +1,10 @@
+string=int(input("Enter your age: "))
+
+if string<10:
+    print("You are a too small.")
+
+elif string<20:
+    print("You are a eligible.")
+
+else:
+    print("You are a too old.")
