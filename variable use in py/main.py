f9e22bff6a42f7b7f99824b01d51adaf1e7682cb
hrishaan2012 <hrishaan2012@gmail.com>
2025-07-24 20:27:28 +0530

    calculate operation

---
+++ variable use in py/main.py
@@ -0,0 +1,14 @@
+def calculate_operations():
+
+    a=int(input("Enter a number :"))
+    b=int(input("Enter another number :"))
+    c=a+b
+    d=a-b
+    e=a*b
+    f=a/b
+    print("Sum:", c)
+    print("Difference:", d)
+    print("Product:", e)
+    print("Quotient:", f)
+
+calculate_operations()
