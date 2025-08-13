e115a48d0f020f539e765f15ee156d71f900a222
hrishaan2012 <hrishaan2012@gmail.com>
2025-08-13 20:51:18 +0530

    time now

---
+++ today clock/main.py
@@ -0,0 +1,7 @@
+from datetime import date, time, datetime
+date_today =date.today()
+time_now= datetime.now()
+
+print("Today's date is:", date_today)
+print("Current time is:", time_now)
+print("Current date and time is:", datetime.now())
