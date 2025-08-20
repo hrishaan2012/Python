e35be6fff091bb5ec4e277525a83e71ed39b54d1
hrishaan2012 <hrishaan2012@gmail.com>
2025-08-20 20:53:45 +0530

    palindrome co

---
+++ palindrome 2/main.py
@@ -0,0 +1,16 @@
+
+def palindrome(r):
+    e=len(r)-1
+    
+    s=0
+    while s<e:
+        if r[s] != r[e]:
+            return False
+        s += 1
+        e -= 1
+    return True
+r=(4, 5, 6, 5, 4)
+if palindrome(r):
+        print("The tuple is a palindrome")
+else:
+        print("The tuple is not a palindrome")
