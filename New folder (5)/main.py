d5a54b62598804fd89220aa2c31c2a82ad41aabc
hrishaan2012 <hrishaan2012@gmail.com>
2025-08-27 20:27:19 +0530

    dict

---
+++ New folder (5)/main.py
@@ -0,0 +1,17 @@
+my_set={1,2,3}
+print(my_set)
+
+my_set=(1.0,"hello",(1,2,3))
+print(my_set)
+
+my_set={1, 2, 3, 4, 3, 2}
+print(my_set)
+
+my_set=set([1, 2, 3, 2])
+print(my_set)
+
+num_set=set( [0, 1, 3, 4, 5])
+print(num_set)
+num_set.pop()
+print("The output is:")
+print(num_set)
