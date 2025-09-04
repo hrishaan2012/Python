e32905133db374b89cbedbcf4e195837bdc98906
hrishaan2012 <hrishaan2012@gmail.com>
2025-09-04 20:42:57 +0530

    CAR

---
+++ grade/car.py
@@ -0,0 +1,8 @@
+class vehicle:
+    def __init__(self,Maxspeed,Milege):
+        self.Maxspeed=Maxspeed
+        self.Milege=Milege
+
+car=vehicle(240,18)
+print("Max speed of the car is",car.Maxspeed)
+print("Milege of the car is",car.Milege)
