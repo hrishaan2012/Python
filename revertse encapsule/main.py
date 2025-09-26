5234d526d1ee6dae9f76d43414f85dfe6e53dd9c
hrishaan2012 <hrishaan2012@gmail.com>
2025-09-26 20:43:28 +0530

    revrsed

---
+++ revertse encapsule/main.py
@@ -0,0 +1,8 @@
+class reverse():
+    def __init__(self,word_s):
+        self=word_s
+    def reverse(self):
+        return self[::-1]
+    word=input("Enter a word: ")
+    obj=reverse(word)
+    print("The reversed word is:",obj)
